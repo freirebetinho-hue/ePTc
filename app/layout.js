afabc9cce5e89fe4e57676500ad62f61fdc565ff
freirebetinho-hue <freire.betinho@gmail.com>
2026-09-01 14:23:37 -0300

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'BPC Busca de Áreas',
+  description: 'Agente de prospecção imobiliária, formação de áreas e análise de renda'
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="pt-BR"><body>{children}</body></html>;
+}
